7d9b6a6e022816e1bb5f4cbed57b9c59d14a78b2
siangyeh8818 <siangyeh8818@gmail.com>
2019-03-05 17:24:36 +0800

    fix docker-entrypoint.sh

--- keeplive-connection-nginx/image-Dockerfile/nginx-multinode-tcp/docker-entrypoint.sh
+++ keeplive-connection-nginx/image-Dockerfile/nginx-multinode-tcp/docker-entrypoint.sh
@@ -10,6 +10,9 @@
 STREAM_INFLUX=""
 STREAM_PROMETHEUS_SYSTEM=""
 STREAM_PROMETHEUS_OPENFAAS=""
+STREAM_PROMETHEUS_MONITOR=""
+STREAM_PROMETHEUS_CEPH=""
+
 OIFS="$IFS"
 IFS=','
 read -a new_string <<< "${K8SWORKIP}"
@@ -19,7 +22,7 @@ do
    temp="server $i:$NGINXPORT weight=5 max_fails=1 fail_timeout=10s;\n" 
    STREAM_INFLUX="${STREAM_INFLUX}           ${temp}"
 done
-
+sleep 1s
 OIFS="$IFS"
 IFS=','
 read -a new_string <<< "${K8SWORKIP}"
@@ -29,7 +32,7 @@ do
    temp="server $i:$NGINXPORT2 weight=5 max_fails=1 fail_timeout=10s;\n"
    STREAM_PROMETHEUS_SYSTEM="${STREAM_PROMETHEUS_SYSTEM}           ${temp}"
 done
-
+sleep 1s
 OIFS="$IFS"
 IFS=','
 read -a new_string <<< "${K8SWORKIP}"
@@ -39,10 +42,32 @@ do
    temp="server $i:$NGINXPORT3 weight=5 max_fails=1 fail_timeout=10s;\n"
    STREAM_PROMETHEUS_OPENFAAS="${STREAM_PROMETHEUS_OPENFAAS}           ${temp}"
 done
-
+sleep 1s
+OIFS="$IFS"
+IFS=','
+read -a new_string <<< "${K8SWORKIP}"
+IFS="$OIFS"
+for i in "${new_string[@]}"
+do
+   temp="server $i:$NGINXPORT4 weight=5 max_fails=1 fail_timeout=10s;\n"
+   STREAM_PROMETHEUS_MONITOR="${STREAM_PROMETHEUS_MONITOR}           ${temp}"
+done
+sleep 1s
+OIFS="$IFS"
+IFS=','
+read -a new_string <<< "${K8SWORKIP}"
+IFS="$OIFS"
+for i in "${new_string[@]}"
+do
+   temp="server $i:$NGINXPORT5 weight=5 max_fails=1 fail_timeout=10s;\n"
+   STREAM_PROMETHEUS_CEPH="${STREAM_PROMETHEUS_CEPH}           ${temp}"
+done
+sleep 1s
 sed -i s"/<UPSTREAM_CONTENT1>/$STREAM_INFLUX/g" /etc/nginx/nginx.conf
 sed -i s"/<UPSTREAM_CONTENT2>/$STREAM_PROMETHEUS_SYSTEM/g" /etc/nginx/nginx.conf
 sed -i s"/<UPSTREAM_CONTENT3>/$STREAM_PROMETHEUS_OPENFAAS/g" /etc/nginx/nginx.conf
+sed -i s"/<UPSTREAM_CONTENT4>/$STREAM_PROMETHEUS_MONITOR/g" /etc/nginx/nginx.conf
+sed -i s"/<UPSTREAM_CONTENT5>/$STREAM_PROMETHEUS_CEPH/g" /etc/nginx/nginx.conf
 sed -i s"/<CONNECTION_LIMIT>/$CONNECTION_LIMIT/g" /etc/nginx/nginx.conf
 sed -i s"/<DESTINATION_PORT>/$DESTINATION_PORT/g" /etc/nginx/nginx.conf
 sed -i s"/<NGINXPORT1>/$NGINXPORT/g" /etc/nginx/nginx.conf
@@ -50,6 +75,8 @@ sed -i s"/<KEEPLOVE_VALUE>/$KEEPLOVE_VALUE/g" /etc/nginx/nginx.conf
 sed -i s"/<NGINX_WORKER>/$NGINX_WORKER/g" /etc/nginx/nginx.conf
 sed -i s"/<NGINXPORT2>/$NGINXPORT2/g" /etc/nginx/nginx.conf
 sed -i s"/<NGINXPORT3>/$NGINXPORT3/g" /etc/nginx/nginx.conf
+sed -i s"/<NGINXPORT4>/$NGINXPORT4/g" /etc/nginx/nginx.conf
+sed -i s"/<NGINXPORT5>/$NGINXPORT5/g" /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 nginx -t
